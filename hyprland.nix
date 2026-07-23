d31eabb719790faf68a54b1dcee6a4c20e3a8041
Samuel John <samuel.john.codes@gmail.com>
2026-07-23 10:13:13 +0530

    feat(in progress): added hyprpolkitagent and hyprlauncher

--- hyprland.nix
+++ hyprland.nix
@@ -1,20 +1,6 @@
-{ ... }: {
+{ pkgs, ... }: {
   environment.systemPackages = with pkgs; [
     hyprlauncher
     hyprpolkitagent
   ];
-
-  systemd.user.services.hyprpolkitagent = {
-    description = "Hyprland PolicyKit Agent";
-    wantedBy = [ "graphical-session.target" ];
-    wants = [ "graphical-session.target" ];
-    after = [ "graphical-session.target" ];
-    serviceConfig = {
-      Type = "simple";
-      ExecStart = "${pkgs.lib.getExe pkgs.hyprpolkitagent}";
-      Restart = "on-failure";
-      RestartSec = 1;
-      TimeoutStopSec = 10;
-    };
-  };
 }
