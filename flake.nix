75bbf5601ac37dd5acc3e9c9b224a50a9c3bd5f9
Samuel John <samuel.john.codes@gmail.com>
2026-08-14 19:50:08 +0530

    add home-manager

--- flake.nix
+++ flake.nix
@@ -2,15 +2,33 @@
   description = "A very basic flake";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-26.05";
+      # The `follows` keyword in inputs is used for inheritance.
+      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
+      # the `inputs.nixpkgs` of the current flake,
+      # to avoid problems caused by different versions of nixpkgs.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     noctalia = {
       url = "github:noctalia-dev/noctalia";
       inputs.nixpkgs.follows = "nixpkgs"; # this line is optional, prevents downloading two versions of nixpkgs but disables cache
     };
   };
-  outputs = { self, nixpkgs, ... } @ inputs: {
+  outputs = { self, nixpkgs, home-manager, ... } @ inputs: {
     nixosConfigurations.nixos-home-hp = nixpkgs.lib.nixosSystem {
       specialArgs = { inherit inputs; };
-      modules = [ ./system/configuration.nix ];
+      modules = [
+        ./system/configuration.nix
+
+        home-manager.nixosModules.home-manager {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.extraSpecialArgs = { inherit inputs; };
+
+          home-manager.users.samuel-john = import ./home.nix;
+        }
+      ];
     };
   };
 }
---
+++ home.nix
@@ -0,0 +1,44 @@
+{ ... }: {
+  home.username = "samuel-john";
+  home.homeDirectory = "/home/samuel-john";
+
+  programs.git = {
+    enable = true;
+    settings = {
+      user = {
+        name = "Samuel John";
+        email = "samuel.john.codes@gmail.com";
+      };
+      core = {
+        compression = 9;
+        whitespace = "error";
+        preloadindex = true;
+      };
+      credential = {
+        helper = "cache";
+      };
+      status = {
+        branch = true;
+        showStash = true;
+        showUntrackedFiles = "all";
+      };
+      diff = {
+        interHunkContext = 10;
+      };
+      push = {
+        autoSetupRemote = true;
+        default = "current";
+      };
+      pull = {
+        default = "current";
+        rebase = true;
+      };
+      rebase = {
+        autoStash = true;
+        missingCommitsCheck = "warn";
+      };
+    };
+  };
+
+  home.stateVersion = "26.05";
+}
