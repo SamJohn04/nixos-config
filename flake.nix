c36e0d77f56763cb82e3ee3115978a9d967067cc
Samuel John <samuel.john.codes@gmail.com>
2026-08-14 09:47:09 +0530

    switch to more stable version

--- flake.nix
+++ flake.nix
@@ -1,16 +1,16 @@
 {
-    description = "A very basic flake";
-    inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-        noctalia = {
-          url = "github:noctalia-dev/noctalia";
-          inputs.nixpkgs.follows = "nixpkgs"; # this line is optional, prevents downloading two versions of nixpkgs but disables cache
-        };
+  description = "A very basic flake";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    noctalia = {
+      url = "github:noctalia-dev/noctalia";
+      inputs.nixpkgs.follows = "nixpkgs"; # this line is optional, prevents downloading two versions of nixpkgs but disables cache
     };
-    outputs = { self, nixpkgs, ... } @ inputs: {
-        nixosConfigurations.nixos-home-hp = nixpkgs.lib.nixosSystem {
-            specialArgs = { inherit inputs; };
-            modules = [ ./configuration.nix ];
-        };
+  };
+  outputs = { self, nixpkgs, ... } @ inputs: {
+    nixosConfigurations.nixos-home-hp = nixpkgs.lib.nixosSystem {
+      specialArgs = { inherit inputs; };
+      modules = [ ./configuration.nix ];
     };
+  };
 }
